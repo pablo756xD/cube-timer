279ba25c89be15f49e45ef28303c97da9f370570
pablo756 <92185570+pablo756xD@users.noreply.github.com>
2024-05-23 23:48:55 -0600

    simplified version of the code, removed buggy avg5
    avg will be added in future version when i can get it working properly

--- timer.c
+++ timer.c
@@ -7,8 +7,8 @@
 
 #define MAX 6
 #define MAXM 3
-
-
+#define SIZE1 5
+#define SIZE2 12
  
 int kbhit(void)
 {
@@ -37,7 +37,6 @@ int kbhit(void)
   return 0;
 }
 
-
 // timer functions
 
 void scramble(void){
@@ -61,7 +60,6 @@ void scramble(void){
   printf("\n");
 }
 
-
 float timer(){
   float time = 0.00;
   printf("%s", "type Enter Key to start the timer...");
@@ -79,36 +77,26 @@ float timer(){
   return time;
 }
 
-void append(float time, float *arr, int *num){
+void append(float time, float *arr, int *num, int size){
   arr[*num] = time;
   (*num)++;
-  if(*num > 4){
+  if(*num > size -1){
     *num = 0;   
   }
 
 }
 
-void push(float time, float *arr, int *num){
-  arr[0] = time;
-  if(*num < 5){
-    (*num)++;
+void print_array(float *arr, int size, char message[]){
+  printf("%s:", message);
+  for(int i = 0; i <= size - 1;i++){
+    printf(" %.2f ", arr[i]);
   }
-
-}
-
-void last_solves(float *avg5){
-  printf("%s","Last solves:  ");
-  for(int i = 0;i < 5;i++){
-    printf("%.2f  ", avg5[i]);
-  }
-
   printf("\n");
-
 }
 
 void sort(float *avg5){
   int size = 5;	
-  for(int i = 0;i < size - 1; i++){
+  for(int i = 0;i < size; i++){
     for(int j = 0;j < size - i - 1;j++){
       if(avg5[j] > avg5[j+1]){
         float temp = avg5[j];
@@ -117,15 +105,6 @@ void sort(float *avg5){
       }
     }
   }
-}
-
-void foreach(float *avg5){ 
- printf("%s","Debug sort function:  ");
-  for(int i = 0;i < 5;i++){
-    printf("%.2f  ", avg5[i]);
-  }
-
-  printf("\n");
 }
 
 void average(float *arr, int *num){
@@ -137,27 +116,24 @@ void average(float *arr, int *num){
 
 }
 
-void loop(float *avg5, float *savg5){
+void loop(float *avg5){
   int counter = 0;	 
   int counter2 = 0;
+  int counter3 = 0;
   while(1){
     scramble();
     float current_time = timer();
-    append(current_time, avg5,&counter);
-    push(current_time, savg5,&counter2);
-    last_solves(avg5);
-    sort(savg5);
-    average(savg5, &counter2);
+    append(current_time, avg5,&counter, SIZE1);
+    print_array(avg5,SIZE1,"last solves");
   }
 
 }
-
 
 int main(){
   float average_of_five[5] = {0};
   float sorted_of_five[5] = {0};
   srand(time(NULL));
-  loop(average_of_five,sorted_of_five);
+  loop(average_of_five);
   return 0;
 
 }
